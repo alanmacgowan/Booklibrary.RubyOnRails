7184cac3731605ccd5fd5bfc541f5e326316c679
Alan Macgowan <alanmacgowan@gmail.com>
2019-08-20 15:25:25 -0300

    added validations, modified layout

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,13 @@
 class Author < ApplicationRecord
+    after_initialize :set_defaults
     has_many :books, dependent: :destroy
+
+    def full_name
+        "#{last_name}, #{first_name}"
+    end
+    
+    private
+    def set_defaults
+        self.country ||= "Argentina"
+    end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,8 @@
 class Book < ApplicationRecord
+  validates :title,
+            presence: true
+
   belongs_to :publisher
   belongs_to :author
+
 end
